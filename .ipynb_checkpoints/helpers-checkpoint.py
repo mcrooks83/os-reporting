47c9866ef3c5072d2b3536bc44fad225a9d7c629
Mike Crooks <mikecrooks83@gmail.com>
2024-10-02 22:22:24 +0300

    clustering

---
+++ .ipynb_checkpoints/helpers-checkpoint.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def read_csv_to_dataframe(path):
+    df = pd.read_csv(path)
+    acc_df = df[["Acc_X", "Acc_Y", "Acc_Z"]]
+    return acc_df
+
+def read_metrics_csv_to_data_frame(path):
+    df= pd.read_csv(path)
+    return df
+    
